ae66bcc9ed80103a5ef0cda6b4d2f4c19359ce6b
Kiryong Ha <krha@cmu.edu>
2012-01-19 11:06:45 -0500

    Ballooning Test

--- src/Script/cloudlet.py
+++ src/Script/cloudlet.py
@@ -12,7 +12,9 @@ import telnetlib
 import socket
 import pylzma
 
-VM_MEMORY = "2048"
+VM_MEMORY = 2048
+VCPU_NUMBER = 1
+BALLOON_MEM_SIZE = 256
 VNC_VIEWER = "/home/krha/Cloudlet/src/Script/vnc_viewer.py"
 PORT_FORWARDING = "-redir tcp:9876::9876 -redir tcp:2222::22 -redir tcp:19092::9092 -redir tcp:6789::6789"
 
@@ -49,7 +51,7 @@ def compare_same(filename1, filename2):
     print '[INFO] checking validity of generated file'
     compare = filecmp.cmp(filename1, filename2)
     if compare == False:
-        print '[ERROR] %s != %s' % (os.path.basename(filename1), os.path.basename(filename2))
+        print >> sys.stderr, '[ERROR] %s != %s' % (os.path.basename(filename1), os.path.basename(filename2))
         return False
     else:
         print '[INFO] SUCCESS to recover'
@@ -92,7 +94,7 @@ def decomp_lzma(inputname, outputname):
 
 def create_overlay(base_image, base_mem):
     # generate overlay VM(disk + memory) from Base VM
-    vm_name = os.path.basename(base_image).split('.')[0]
+    vm_name = os.path.basename(base_image)
     vm_path = os.path.dirname(base_mem)
     overlay_disk = os.path.join(os.getcwd(), vm_name) + '_overlay.qcow2'
     overlay_mem = os.path.join(os.getcwd(), vm_name) + '_overlay.mem'
@@ -104,40 +106,47 @@ def create_overlay(base_image, base_mem):
     print '[INFO] run Base Image to generate memory snapshot'
     telnet_port = 19823; vnc_port = 2
     run_snapshot(tmp_disk, base_mem, telnet_port, vnc_port, wait_vnc_end=True)
+
+    # shrink down memory size 
+    if VM_MEMORY != BALLOON_MEM_SIZE:
+        ret = run_ballooning(telnet_port, BALLOON_MEM_SIZE)
+        if not ret: 
+            print >> sys.stderr, "[ERROR] Cannot shrink down memory to " + str(BALLOON_MEM_SIZE)
+            return None, None
     # stop and migrate
     run_migration(telnet_port, vnc_port, tmp_mem)
 
     if os.path.exists(tmp_mem) == False:
-        print '[ERROR] new memory snapshot (%s) is not exit' % tmp_mem
+        print >> sys.stderr, '[ERROR] new memory snapshot (%s) is not exit' % tmp_mem
         if os.path.exists(tmp_mem):
             os.remove(tmp_mem)
         if os.path.exists(tmp_disk):
             os.remove(tmp_disk)
-        return None, None, None, None
+        return None, None
 
     prev_time = datetime.now()
     ret = diff_files(base_image, tmp_disk, overlay_disk)
     print '[TIME] time for creating overlay disk : ', str(datetime.now()-prev_time)
-    print '[INFO] (%d)-(%d)=(%d): ' % (os.path.getsize(base_image)/1024/1024, os.path.getsize(tmp_disk)/1024/1024, os.path.getsize(overlay_disk)/1024/1024)
+    print '[INFO] (%d)-(%d)=(%d): ' % (os.path.getsize(base_image), os.path.getsize(tmp_disk), os.path.getsize(overlay_disk))
     if ret == None:
-        print '[ERROR] cannot create overlay disk'
+        print >> sys.stderr, '[ERROR] cannot create overlay disk'
         if os.path.exists(tmp_mem):
             os.remove(tmp_mem)
         if os.path.exists(tmp_disk):
             os.remove(tmp_disk)
-        return None, None, None, None
+        return None, None
     
     prev_time = datetime.now()
     ret = diff_files(base_mem, tmp_mem, overlay_mem)
     print '[TIME] time for creating overlay memory : ', str(datetime.now()-prev_time)
-    print '[INFO] (%d)-(%d)=(%d): ' % (os.path.getsize(base_mem)/1024/1024, os.path.getsize(tmp_mem)/1024/1024, os.path.getsize(overlay_mem)/1024/1024)
+    print '[INFO] (%d)-(%d)=(%d): ' % (os.path.getsize(base_mem), os.path.getsize(tmp_mem), os.path.getsize(overlay_mem))
     if ret == None:
-        print '[ERROR] cannot create overlay_mem'
+        print >> sys.stderr, '[ERROR] cannot create overlay_mem'
         if os.path.exists(tmp_mem):
             os.remove(tmp_mem)
         if os.path.exists(tmp_disk):
             os.remove(tmp_disk)
-        return None, None, None, None
+        return None, None
 
     # compression
     comp_disk = overlay_disk + '.lzma'
@@ -149,6 +158,7 @@ def create_overlay(base_image, base_mem):
     os.remove(tmp_disk)
     os.remove(overlay_disk)
     os.remove(overlay_mem)
+
     return comp_disk, comp_mem
 
 def recover_snapshot(base_img, base_mem, comp_img, comp_mem):
@@ -196,7 +206,6 @@ def telnet_connection_waiting(telnet_port):
         tn.close()
 
     #print "Socket connection time : ", str(datetime.now()-start_time)
-
     start_time = datetime.now()
     if is_connected:
         for i in xrange(200):
@@ -227,12 +236,13 @@ def run_snapshot(disk_image, memory_image, telnet_port, vnc_port, wait_vnc_end):
     command_str = "kvm -hda "
     command_str += disk_image
     if telnet_port != 0 and vnc_port != -1:
-        command_str += " -m " + VM_MEMORY + " -monitor telnet:localhost:" + str(telnet_port) + ",server,nowait -enable-kvm -net nic -net user -serial none -parallel none -usb -usbdevice tablet " + PORT_FORWARDING
-        #command_str += " -vnc :" + str(vnc_port) + " "
+        command_str += " -m " + str(VM_MEMORY) + " -monitor telnet:localhost:" + str(telnet_port) + ",server,nowait -enable-kvm -net nic -net user -serial none -parallel none -usb -usbdevice tablet " + PORT_FORWARDING
+        command_str += " -vnc :" + str(vnc_port)
+        #command_str += " -vnc unix:" + vnc_file
+        command_str += " -smp " + str(VCPU_NUMBER)
         command_str += " -balloon virtio"
-        command_str += " -vnc unix:" + vnc_file
     else:
-        command_str += " -m " + VM_MEMORY + " -enable-kvm -net nic -net user -serial none -parallel none -usb -usbdevice tablet -redir tcp:2222::22"
+        command_str += " -m " + str(VM_MEMORY) + " -enable-kvm -net nic -net user -serial none -parallel none -usb -usbdevice tablet -redir tcp:2222::22"
     command_str += " -incoming \"exec:cat " + memory_image + "\""
     print '[INFO] Run snapshot..'
     print command_str
@@ -254,8 +264,8 @@ def run_snapshot(disk_image, memory_image, telnet_port, vnc_port, wait_vnc_end):
 
     if ret:
         # Run VNC
-        vnc_process = subprocess.Popen(VNC_VIEWER + " " + vnc_file, shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE)
-        # vnc_process = subprocess.Popen("gvncviewer localhost:" + str(vnc_port), shell=True)
+        # vnc_process = subprocess.Popen(VNC_VIEWER + " " + vnc_file, shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+        vnc_process = subprocess.Popen("gvncviewer localhost:" + str(vnc_port), shell=True)
         if wait_vnc_end:
             ret = vnc_process.wait()
 
@@ -279,7 +289,7 @@ def run_migration(telnet_port, vnc_port, mig_path):
     tn = telnetlib.Telnet('localhost', telnet_port)
     tn.write("stop\n")
     ret = tn.read_until("(qemu)", 10)
-    print 'waiting telnet ', ret
+    # print 'waiting telnet ', ret
     tn.write(migration_cmd)
     while (1):
         time.sleep(1)
@@ -287,8 +297,59 @@ def run_migration(telnet_port, vnc_port, mig_path):
         if tn_read.find("(qemu)"):
             break;
     tn.write("quit\n")
-    ret = tn.read_until("(qemu)", 10)
     tn.close()
+
+
+def run_ballooning(telnet_port, target_mem_size):
+    # original mem size
+    tn = telnetlib.Telnet('localhost', telnet_port)
+    tn.read_until("(qemu)", 10)
+    mem_info_cmd = "info balloon\n"
+    tn.write(mem_info_cmd)
+    ret = tn.read_until("(qemu)", 10)
+    ret = ret.split('\n')[1]
+    if not len(ret.split("actual=")) == 2:
+        return False;
+
+    original_mem_size = int(ret.split("actual=")[1].strip())
+    tn.close()
+
+    ret = set_balloon_size(telnet_port, target_mem_size)
+    if ret:
+        ret = set_balloon_size(telnet_port, original_mem_size)
+        return ret
+    else:
+        return False
+
+
+def set_balloon_size(telnet_port, target_mem_size):
+    start_time = datetime.now()
+    tn = telnetlib.Telnet('localhost', telnet_port)
+    tn.read_until("(qemu)", 10)
+
+    # ballooning to target size
+    balloon_cmd = "balloon " + str(target_mem_size) + "\n"
+    tn.write(balloon_cmd)
+    # print "writing ballon command : " + str(datetime.now())
+    tn.read_until("(qemu)", 20)
+    # print "returned : " + str(datetime.now())
+
+    for i in xrange(200):
+        try:
+            print "waiting for balloon memory size to %s" % (target_mem_size)
+            tn.write('info balloon\n')
+            ret = tn.read_until("(qemu)", 1)
+            # print "request ret : %s, %s\n" % (ret, datetime.now())
+            if ret.find(str(target_mem_size)) != -1:
+                print "success to balloon %s(MB) at %s" % (target_mem_size, str(datetime.now()-start_time))
+                tn.close()
+                return True
+        except socket.timeout:
+            pass
+        time.sleep(1)
+
+    tn.close()
+    return False
 
 
 #stop VM using telnet qemu port
@@ -297,29 +358,45 @@ def stop_vm(telnet_port):
     tn.write("stop\n")
     ret = tn.read_until("(qemu)", 10)
     tn.write("quit\n")
-    ret = tn.read_until("(qemu)", 2)
+    tn.read_until("(qemu)", 10)
     tn.close()
 
 
 def create_base(imagefile):
     if os.path.exists(imagefile) == False:
-        print '[ERROR] %s is not exist' % imagefile
+        print >> sys.stderr, '[ERROR] %s is not exist' % imagefile
         return None
 
+    info_tag = '.base.' + str(VCPU_NUMBER) + 'cpu.' + str(VM_MEMORY) + "mem." + str(BALLOON_MEM_SIZE) + "ballloon"
     vm_name = os.path.basename(imagefile).split('.')[0]
     vm_path = os.path.dirname(imagefile)
-    base_image = os.path.join(vm_path, vm_name) + '_base.qcow2'
+    base_image = os.path.join(vm_path, vm_name) + info_tag + '.qcow2'
+
+    # check existing file first
+    if os.path.exists(base_image):
+        message = "(%s) is exist. Are you sure to overwrite?(y/N) " % (base_image)
+        ret = raw_input(message)
+        if str(ret).lower() != 'y':
+            sys.exit(1)
+
     command_str = 'qemu-img create -f qcow2 -b ' + imagefile + ' ' + base_image
     ret = commands.getoutput(command_str)
     print '[INFO] run Base Image to generate memory snapshot'
     telnet_port = 12123; vnc_port = 3
     run_image(base_image, telnet_port, vnc_port)
 
+    base_mem = os.path.join(vm_path, vm_name) + info_tag + '.mem'
+
+    # shrink down memory size with ballooning
+    if VM_MEMORY != BALLOON_MEM_SIZE:
+        ret = run_ballooning(telnet_port, BALLOON_MEM_SIZE)
+        if not ret:
+            print >> sys.stderr, "[ERROR] Cannot shrink down memory to " + str(BALLOON_MEM_SIZE)
+            return None, None
     # stop and migrate
-    base_mem = os.path.join(vm_path, vm_name) + '_base.mem'
     run_migration(telnet_port, vnc_port, base_mem)
     if os.path.exists(base_mem) == False:
-        print '[ERROR] base memory snapshot (%s) is not exit' % base_mem
+        print >> sys.stderr, '[ERROR] base memory snapshot (%s) is not exit' % base_mem
         return None, None
 
     return base_image, base_mem
@@ -328,18 +405,20 @@ def run_image(disk_image, telnet_port, vnc_port):
     command_str = "kvm -hda "
     command_str += disk_image
     if telnet_port != 0 and vnc_port != -1:
-        command_str += " -m " + VM_MEMORY + " -monitor telnet:localhost:" + str(telnet_port) + ",server,nowait -enable-kvm -net nic -net user -serial none -parallel none -usb -usbdevice tablet -redir tcp:9876::9876"
-        command_str += " -vnc :" + str(vnc_port) + " "
+        command_str += " -m " + str(VM_MEMORY) + " -monitor telnet:localhost:" + str(telnet_port) + ",server,nowait -enable-kvm -net nic -net user -serial none -parallel none -usb -usbdevice tablet -redir tcp:9876::9876"
+        command_str += " -vnc :" + str(vnc_port)
+        command_str += " -smp " + str(VCPU_NUMBER)
+        command_str += " -balloon virtio"
     else:
-        command_str += " -m " + VM_MEMORY + " -enable-kvm -net nic -net user -serial none -parallel none -usb -usbdevice tablet -redir tcp:2222::22"
+        command_str += " -m " + str(VM_MEMORY) + " -enable-kvm -net nic -net user -serial none -parallel none -usb -usbdevice tablet -redir tcp:2222::22"
     print '[DEBUG] command : ' + command_str
-    process = subprocess.Popen(command_str, shell=True)
+    subprocess.Popen(command_str, shell=True)
 
     # Run VNC and wait until user finishes working
     time.sleep(3)
     vnc_process = subprocess.Popen("gvncviewer localhost:" + str(vnc_port), shell=True)
     #vnc_process = subprocess.Popen(VNC_VIEWER + " " + vnc_file, shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE)
-    ret = vnc_process.wait()
+    vnc_process.wait()
 
 
 def print_usage(program_name):
