12080af7c9ac67fcabda1c535a37d50a84b69633
Kiryong Ha <krha@cmu.edu>
2012-02-03 07:51:36 -0500

    Adding License Information

--- src/Client/src/edu/cmu/cs/cloudlet/android/util/ConnectionLog.java
+++ src/Client/src/edu/cmu/cs/cloudlet/android/util/ConnectionLog.java
@@ -1,3 +1,17 @@
+//
+// Copyright (C) 2011-2012 Carnegie Mellon University
+//
+// This program is free software; you can redistribute it and/or modify it
+// under the terms of version 2 of the GNU General Public License as published
+// by the Free Software Foundation.  A copy of the GNU General Public License
+// should have been distributed along with this program in the file
+// LICENSE.GPL.
+
+// This program is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+// or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+// for more details.
+//
 package edu.cmu.cs.cloudlet.android.util;
 
 import java.io.BufferedWriter;
