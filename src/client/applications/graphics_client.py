327ad856f303edf5135b66ff12293e3cdead9d93
Kiryong Ha <krha@cmu.edu>
2012-04-11 19:53:32 -0400

    Client ID + Server ACK + Data

--- src/client/applications/graphics_client.py
+++ src/client/applications/graphics_client.py
@@ -20,7 +20,14 @@ import socket
 from optparse import OptionParser
 import time
 import struct
-import math
+from select import select
+from threading import Thread
+
+token_id = 0
+total_recv_number = 0;
+
+sender_time_stamps = []
+receiver_time_stamps = []
 
 def recv_all(sock, size):
     data = ''
@@ -49,7 +56,98 @@ def process_command_line(argv):
     return settings, args
 
 
-def send_request(address, port, input_data):
+def recv_data(sock):
+    global token_id
+    global total_recv_number
+    # recv
+    print "index\tstart\tend\tduration\tjitter\tout"
+    try:
+        while True:
+            data = sock.recv(4)
+            client_id = struct.unpack("!I", data)[0]
+            data = sock.recv(4)
+            server_token_id = struct.unpack("!I", data)[0]
+            data = sock.recv(4)
+            ret_size = struct.unpack("!I", data)[0]
+            #print "Client ID : %d, Recv size : %d" % (client_id, ret_size)
+            token_id = server_token_id
+            #receiver_time_stamps.append((client_id, time.time()))
+            
+            if not ret_size == 0:
+                ret_data = recv_all(sock, ret_size)
+                total_recv_number = total_recv_number + 1
+                if not ret_size == len(ret_data):
+                    sys.stderr.write("Error, returned value size : %d" % (len(ret_data)))
+                    sys.exit(1)
+            else:
+                sys.stderr.write("Error, return size must not be zero")
+                sys.exit(1)
+
+            # print result
+            '''
+            end_time_request = time.time() * 1000.0
+            prev_duration = current_duration
+            current_duration = end_time_request-start_time_request
+            if prev_duration == -1: # fisrt response
+                print "%d\t%014.2f\t%014.2f\t%014.2f\t0\t%014.2f" % (index, start_time_request,\
+                        end_time_request, \
+                        end_time_request-start_time_request,\
+                        len(ret_data))
+                        
+            else:
+                print "%d\t%014.2f\t%014.2f\t%014.2f\t%014.2f\t%014.2f" % (index, round(start_time_request, 3), \
+                        end_time_request, \
+                        current_duration, \
+                        math.fabs(current_duration-prev_duration), \
+                        len(ret_data))
+            '''
+
+    except socket.error:
+        print "Socket Closed and Closing Recv Thread"
+
+
+def send_request(sock, input_data):
+    global token_id
+
+    # send requests
+    if input_data:
+        loop_length = len(input_data)
+    else:
+        loop_length = 1000
+
+    index = 0
+    last_sent_time = 0
+    while True:
+        read_ready, write_ready, others = select([sock], [sock], [])
+        if sock in write_ready:
+            if index == loop_length-1:
+                break;
+
+            # send acc data
+            if (time.time() - last_sent_time) > 0.02:
+                if input_data:
+                    if len(input_data[index].split("  ")) != 3:
+                        print "Error input : %s" % input_data[index]
+                        continue
+                    x_acc = float(input_data[index].split("  ")[1])
+                    y_acc = float(input_data[index].split("  ")[2])
+                else:
+                    x_acc = -9.0 
+                    y_acc = -1.0
+
+                #sender_time_stamps.append((index, time.time()))
+                sent_size = sock.send(struct.pack("!iiff", index, token_id, x_acc, y_acc))
+                last_sent_time = time.time()
+                index += 1
+                print "[%03d/%d] Sent ACK(%d), acc (%f, %f)" % (index, loop_length, token_id, x_acc, y_acc)
+                if not sent_size == 16:
+                    sys.stderr.write("Error, send wrong size of acc data: %d" + sent_size)
+                    sys.exit(1)
+
+    sock.close()
+
+
+def connect(address, port, input_data):
     # connection
     try:
         print "Connecting to (%s, %d).." % (address, port)
@@ -61,69 +159,19 @@ def send_request(address, port, input_data):
         sys.stderr.write("Error, %s\n" % msg[1])
         sys.exit(1)
 
-    # send requests
-    current_duration = -1
-    print "index\tstart\tend\tduration\tjitter\tout"
-    start_time = time.time()
+    sender = Thread(target=send_request, args=(sock,input_data))
+    recv = Thread(target=recv_data, args=(sock,))
 
-    if input_data:
-        loop_length = len(input_data)
-    else:
-        loop_length = 1000
+    start_client_time = time.time()
+    sender.start()
+    recv.start()
 
-    for index in xrange(loop_length):
-        start_time_request = time.time() * 1000.0
+    print "Waiting for end of acc data transmit"
+    sender.join()
+    duration = time.time() - start_client_time
 
-        # send acc data
-        if input_data:
-            if len(input_data[index].split("  ")) != 3:
-                print "Error input : %s" % input_data[index]
-                continue
-            x_acc = float(input_data[index].split("  ")[1])
-            y_acc = float(input_data[index].split("  ")[2])
-        else:
-            x_acc = -9.0 
-            y_acc = -1.0
-
-        sent_size = sock.send(struct.pack("!ff", x_acc, y_acc))
-        #print "Sent acc (%f, %f)" % (x_acc, y_acc)
-        if not sent_size == 8:
-            sys.strerr.write("Error, send wrong size of acc data: %d" + sent_size)
-            sys.exit(1)
-        
-        # recv
-        data = sock.recv(4)
-        ret_size = struct.unpack("!I", data)[0]
-        #print "Recv size : %d" % (ret_size)
-        
-        if not ret_size == 0:
-            ret_data = recv_all(sock, ret_size)
-            if not ret_size == len(ret_data):
-                sys.stderr.write("Error, returned value size : %d" % (len(ret_data)))
-                sys.exit(1)
-        else:
-            sys.stderr.write("Error, return size must not be zero")
-            sys.exit(1)
-
-        # print result
-        end_time_request = time.time() * 1000.0
-        prev_duration = current_duration
-        current_duration = end_time_request-start_time_request
-
-        if prev_duration == -1: # fisrt response
-            print "%d\t%014.2f\t%014.2f\t%014.2f\t0\t%014.2f" % (index, start_time_request,\
-                    end_time_request, \
-                    end_time_request-start_time_request,\
-                    len(ret_data))
-                    
-        else:
-            print "%d\t%014.2f\t%014.2f\t%014.2f\t%014.2f\t%014.2f" % (index, round(start_time_request, 3), \
-                    end_time_request, \
-                    current_duration, \
-                    math.fabs(current_duration-prev_duration), \
-                    len(ret_data))
-
-    print "Total time : %014.2f" % (time.time()-start_time)
+    print "Total Time: %s, Average FPS: %5.2f, Sent ACC #: %d" % \
+            (str(duration), total_recv_number/duration, len(input_data))
 
 def main(argv=None):
     global LOCAL_IPADDRESS
@@ -132,7 +180,7 @@ def main(argv=None):
     if settings.input_file and os.path.exists(settings.input_file):
         input_accs = open(settings.input_file, "r").read().split("\n")
 
-    send_request(settings.server_address, settings.server_port, input_accs)
+    connect(settings.server_address, settings.server_port, input_accs)
 
     return 0
 
