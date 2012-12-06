fe156c70ab64c6658ae9204f8e7e9191c184ebaa
Kiryong Ha <krha@cmu.edu>
2012-12-05 22:14:18 -0500

    batch script for client

---
+++ src/client/desktop/batch_client.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+#
+# Elijah: Cloudlet Infrastructure for Mobile Computing
+# Copyright (C) 2011-2012 Carnegie Mellon University
+#
+# This program is free software; you can redistribute it and/or modify it
+# under the terms of version 2 of the GNU General Public License as published
+# by the Free Software Foundation.  A copy of the GNU General Public License
+# should have been distributed along with this program in the file
+# LICENSE.GPL.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+# or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+# for more details.
+#
+
+import time
+import rapid_client
+
+WAIT_TIME = 20
+
+cloudlet_server_ip = "cloudlet.krha.kr"
+cloudlet_server_port = 8021
+rapid_client.synthesis(cloudlet_server_ip, cloudlet_server_port, "moped_access_16", batch=True, wait_time=WAIT_TIME)
+rapid_client.synthesis(cloudlet_server_ip, cloudlet_server_port, "face_access_16", batch=True, wait_time=WAIT_TIME)
+rapid_client.synthesis(cloudlet_server_ip, cloudlet_server_port, "face_access_1024", batch=True, wait_time=WAIT_TIME)
+rapid_client.synthesis(cloudlet_server_ip, cloudlet_server_port, "face_access_1048576", batch=True, wait_time=WAIT_TIME)
+rapid_client.synthesis(cloudlet_server_ip, cloudlet_server_port, "speech_access_16", batch=True, wait_time=WAIT_TIME)
+rapid_client.synthesis(cloudlet_server_ip, cloudlet_server_port, "mar_access_16", batch=True, wait_time=WAIT_TIME)
+rapid_client.synthesis(cloudlet_server_ip, cloudlet_server_port, "graphics_access_16", batch=True, wait_time=WAIT_TIME)
