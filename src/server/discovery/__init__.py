a8c494ecc99296d5911d5074c84b2f7df4413bd9
Kiryong Ha <krha@cmu.edu>
2013-02-02 22:47:59 -0500

    Register client for the client is done It works with RESTful central directory server by sending periodic heart beat.

---
+++ src/server/discovery/__init__.py
@@ -0,0 +1,19 @@
+# Elijah: Cloudlet Infrastructure for Mobile Computing
+# Copyright (C) 2011-2012 Carnegie Mellon University
+#
+# This program is free software; you can redistribute it and/or modify it
+# under the terms of version 2 of the GNU General Public License as published
+# by the Free Software Foundation.  A copy of the GNU General Public License
+# should have been distributed along with this program in the file
+# LICENSE.GPL.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+# or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+# for more details.
+#
+
+import libvirt
+
+assert(libvirt.getVersion() >= 9004) # 0.9.4
+
