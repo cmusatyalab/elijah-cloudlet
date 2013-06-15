9d877a8ea882153702351b43256281bf7b9b58bb
Kiryong Ha <krha@cmu.edu>
2013-06-14 21:29:15 -0400

    change license to Apache Version 2.0

--- src/client/android/src/edu/cmu/cs/cloudlet/android/CloudletActivity.java
+++ src/client/android/src/edu/cmu/cs/cloudlet/android/CloudletActivity.java
@@ -1,16 +1,24 @@
-//
-// Copyright (C) 2011-2012 Carnegie Mellon University
-//
-// This program is free software; you can redistribute it and/or modify it
-// under the terms of version 2 of the GNU General Public License as published
-// by the Free Software Foundation.  A copy of the GNU General Public License
-// should have been distributed along with this program in the file
-// LICENSE.GPL.
+/*
+ *
+ * Cloudlet Infrastructure for Mobile Computing
+ *
+ *   Author: Kiryong Ha <krha@cmu.edu>
+ *
+ *   Copyright (C) 2011-2013 Carnegie Mellon University
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ *
+ */
 
-// This program is distributed in the hope that it will be useful, but
-// WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
-// or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
-// for more details.
 package edu.cmu.cs.cloudlet.android;
 
 import java.io.File;
