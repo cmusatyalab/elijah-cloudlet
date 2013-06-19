1e8c2aff90440b596bd2041c294867ff5089f5ba
Kiryong Ha <krha@cmu.edu>
2013-06-19 18:57:09 -0400

    polishing fabric file

--- fabfile.py
+++ fabfile.py
@@ -61,12 +61,15 @@ def install():
     if sudo("adduser %s libvirtd" % username).failed:
         abort("Cannot add user to libvirtd group")
 
-    # make sure to have fuse support
+    # Make sure to have fuse support
+    # qemu-kvm changes the permission of /dev/fuse, so we revert back the
+    # permission. This bug is fixed from udev-175-0ubuntu26
+    # Please see https://bugs.launchpad.net/ubuntu/+source/udev/+bug/1152718
     if sudo("chmod 1666 /dev/fuse").failed:
         abort("Failed to enable fuse for the user")
-    if sudo("chmod 644 /etc/fuse.conf").failed:
-        abort("failed to enable fuse for the user")
 
-    # (optional) disable EPT support
+    # (Optional) disable EPT support
+    # When you use EPT support with FUSE+mmap, it randomly causes kernel panic.
+    # We're investigating it whether it's Linux kernel bug or not.
     disable_EPT()
     sys.stdout.write("[SUCCESS] VM synthesis code is installed\n")
