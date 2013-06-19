e2279b75a26077d45544ae5f0e16b5f02d513477
Kiryong Ha <krha@cmu.edu>
2013-06-19 12:25:27 -0400

    polish fabric file

--- fabfile.py
+++ fabfile.py
@@ -9,32 +9,43 @@ from fabric.api import task
 from fabric.api import abort
 
 
+def check_support():
+    if run("egrep '^flags.*(vmx|svm)' /proc/cpuinfo > /dev/null").failed:
+        abort("Need hardware VM support (vmx)")
+
+
+def disable_EPT():
+    if run("egrep '^flags.*(ept)' /proc/cpuinfo > /dev/null").failed:
+        return
+    else:
+        # disable EPT
+        sudo('modprobe -r kvm_intel')
+        sudo('modprobe kvm_intel "ept=0"')
+    pass
+
+
 @task
 def localhost():
     env.run = local
     env.warn_only = True
     env.hosts = ['localhost']
 
+
 @task
 def remote():
     env.run = run
     env.warn_only = True
-    env.hosts  = ['some.remote.host']
-
-
-def check_support():
-    if run("egrep '^flags.*(vmx|svm)' /proc/cpuinfo > /dev/null").failed:
-        abort("Need hardware VM support (vmx)")
-
+    env.hosts = ['some.remote.host']
 
 @task
 def install():
     check_support()
+
     # install dependent package
     sudo("apt-get update")
-    if sudo("apt-get install -y qemu-kvm libvirt-bin gvncviewer python-libvirt " + 
-            "python-xdelta3 python-dev openjdk-6-jre liblzma-dev apparmor-utils " + 
-            "libc6-i386 python-pip").failed:
+    if sudo("apt-get install -y qemu-kvm libvirt-bin gvncviewer " +
+            "python-libvirt python-xdelta3 python-dev openjdk-6-jre  " +
+            "liblzma-dev apparmor-utils libc6-i386 python-pip").failed:
         abort("Failed to install libraries")
     if sudo("pip install bson pyliblzma psutil sqlalchemy").failed:
         abort("Failed to install python libraries")
@@ -56,5 +67,6 @@ def install():
     if sudo("chmod 644 /etc/fuse.conf").failed:
         abort("failed to enable fuse for the user")
 
+    # (optional) disable EPT support
+    disable_EPT()
     sys.stdout.write("[SUCCESS] VM synthesis code is installed\n")
-
