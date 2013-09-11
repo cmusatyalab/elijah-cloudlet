1bb1a779dde261f4280b65e61059f0e8241437ce
Kiryong Ha <krha@cmu.edu>
2013-09-11 13:54:04 -0400

    polishing integration

--- src/discovery/register_server/fileshack/urls.py
+++ src/discovery/register_server/fileshack/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls.defaults import patterns, include, url
+from django.contrib.auth.decorators import login_required
 
 urlpatterns = patterns('fileshack.views',
     url(r'^cron/$', 'cron', name='cron'),
